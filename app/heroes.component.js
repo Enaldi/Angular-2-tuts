2702814a54aaf0281403f9d3c1b5554c3b4f30b4
Chris Franchetti Michaels <Chris.FranchettiMichaels@calbt.com>
2016-02-23 17:45:59 -0800

    Completes routing (bug: Heroes list styles lost)

--- app/heroes.component.js
+++ app/heroes.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero-detail.component', './hero.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './hero-detail.component', './hero.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,12 +8,15 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_detail_component_1, hero_service_1;
+    var core_1, router_1, hero_detail_component_1, hero_service_1;
     var HeroesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             },
             function (hero_detail_component_1_1) {
                 hero_detail_component_1 = hero_detail_component_1_1;
@@ -26,7 +29,8 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
             // App component
             //
             HeroesComponent = (function () {
-                function HeroesComponent(_heroService) {
+                function HeroesComponent(_router, _heroService) {
+                    this._router = _router;
                     this._heroService = _heroService;
                     this.title = "Tour of Heros";
                 } // Construct data service => supply the fresh instance of the service when Appcomponent created
@@ -41,17 +45,20 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
                     // console.log(`onSelect() fired - hero: ${hero.name}`);
                     this.selectedHero = hero;
                 };
-                ;
+                HeroesComponent.prototype.gotoDetail = function () {
+                    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+                };
                 HeroesComponent = __decorate([
                     core_1.Component({
                         selector: 'my-heroes',
-                        template: "\n    \t\t\t<h2>My Heroes</h2>\n\t\t\t\t<ul class=\"heroes\">\n\t\t\t\t  <li *ngFor=\"#hero of heroes\" \n\t\t\t\t\t[class.selected]=\"hero === selectedHero\"\n\t\t\t\t  \t(click)=\"onSelect(hero)\"> \n\t\t\t\t    \t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t\t  </li>\n\t\t\t\t</ul>\n\n\t\t\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n    \t\t\t",
-                        styles: ["\n\t\t\t  .selected {\n\t\t\t    background-color: #CFD8DC !important;\n\t\t\t    color: white;\n\t\t\t  }\n\t\t\t  .heroes {\n\t\t\t    margin: 0 0 2em 0;\n\t\t\t    list-style-type: none;\n\t\t\t    padding: 0;\n\t\t\t    width: 10em;\n\t\t\t  }\n\t\t\t  .heroes li {\n\t\t\t    cursor: pointer;\n\t\t\t    position: relative;\n\t\t\t    left: 0;\n\t\t\t    background-color: #EEE;\n\t\t\t    margin: .5em;\n\t\t\t    padding: .3em 0em;\n\t\t\t    height: 1.6em;\n\t\t\t    border-radius: 4px;\n\t\t\t  }\n\t\t\t  .heroes li.selected:hover {\n\t\t\t    color: white;\n\t\t\t  }\n\t\t\t  .heroes li:hover {\n\t\t\t    color: #607D8B;\n\t\t\t    background-color: #EEE;\n\t\t\t    left: .1em;\n\t\t\t  }\n\t\t\t  .heroes .text {\n\t\t\t    position: relative;\n\t\t\t    top: -3px;\n\t\t\t  }\n\t\t\t  .heroes .badge {\n\t\t\t    display: inline-block;\n\t\t\t    font-size: small;\n\t\t\t    color: white;\n\t\t\t    padding: 0.8em 0.7em 0em 0.7em;\n\t\t\t    background-color: #607D8B;\n\t\t\t    line-height: 1em;\n\t\t\t    position: relative;\n\t\t\t    left: -1px;\n\t\t\t    top: -4px;\n\t\t\t    height: 1.8em;\n\t\t\t    margin-right: .8em;\n\t\t\t    border-radius: 4px 0px 0px 4px;\n\t\t\t  }\n\t"],
+                        templateUrl: '/app/templates/heroes.component.html',
+                        styleUrls: ['/app/styles/heroes.component.css'],
                         directives: [hero_detail_component_1.HeroDetailComponent]
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService])
+                    __metadata('design:paramtypes', [(typeof (_a = typeof router_1.Router !== 'undefined' && router_1.Router) === 'function' && _a) || Object, hero_service_1.HeroService])
                 ], HeroesComponent);
                 return HeroesComponent;
+                var _a;
             })();
             exports_1("HeroesComponent", HeroesComponent);
         }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
+import { Router } from 'angular2/router';
 import {Hero} from './hero-interface';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
@@ -8,66 +9,8 @@ import {HeroService} from './hero.service';
 //
 @Component({
     selector: 	'my-heroes',
-    template:	`
-    			<h2>My Heroes</h2>
-				<ul class="heroes">
-				  <li *ngFor="#hero of heroes" 
-					[class.selected]="hero === selectedHero"
-				  	(click)="onSelect(hero)"> 
-				    	<span class="badge">{{hero.id}}</span> {{hero.name}}
-				  </li>
-				</ul>
-
-				<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    			`,
-	styles: [`
-			  .selected {
-			    background-color: #CFD8DC !important;
-			    color: white;
-			  }
-			  .heroes {
-			    margin: 0 0 2em 0;
-			    list-style-type: none;
-			    padding: 0;
-			    width: 10em;
-			  }
-			  .heroes li {
-			    cursor: pointer;
-			    position: relative;
-			    left: 0;
-			    background-color: #EEE;
-			    margin: .5em;
-			    padding: .3em 0em;
-			    height: 1.6em;
-			    border-radius: 4px;
-			  }
-			  .heroes li.selected:hover {
-			    color: white;
-			  }
-			  .heroes li:hover {
-			    color: #607D8B;
-			    background-color: #EEE;
-			    left: .1em;
-			  }
-			  .heroes .text {
-			    position: relative;
-			    top: -3px;
-			  }
-			  .heroes .badge {
-			    display: inline-block;
-			    font-size: small;
-			    color: white;
-			    padding: 0.8em 0.7em 0em 0.7em;
-			    background-color: #607D8B;
-			    line-height: 1em;
-			    position: relative;
-			    left: -1px;
-			    top: -4px;
-			    height: 1.8em;
-			    margin-right: .8em;
-			    border-radius: 4px 0px 0px 4px;
-			  }
-	`],
+    templateUrl: '/app/templates/heroes.component.html',
+	styleUrls: ['/app/styles/heroes.component.css'],
 	directives: [HeroDetailComponent]
 	// No HeroService provider here b/c has been PROMOTED to app.components.
 })
@@ -77,7 +20,10 @@ export class HeroesComponent implements OnInit {
 	public heroes: Hero[];
 	public selectedHero: Hero;
 
-	constructor(private _heroService: HeroService) { } // Construct data service => supply the fresh instance of the service when Appcomponent created
+	constructor(
+		private _router: Router,
+		private _heroService: HeroService
+		) { } // Construct data service => supply the fresh instance of the service when Appcomponent created
 
 	getHeroes() {
 		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -90,8 +36,10 @@ export class HeroesComponent implements OnInit {
 	onSelect(hero: Hero) { 
 		// console.log(`onSelect() fired - hero: ${hero.name}`);
 		this.selectedHero = hero;
-	};
+	}
 
+	gotoDetail() { // On click
+		this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+	}
 
-	
 }
